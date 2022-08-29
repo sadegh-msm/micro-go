bb7606f1ccaac7a66ae5d90abad1c4446ce770b5
mohamad sadegh mohamadi <ms.crysis1380@gmail.com>
2022-08-29 18:26:38 +0430

    completing auth service

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 )
@@ -43,8 +45,54 @@ func (app *Config) HandleAll(w http.ResponseWriter, r *http.Request) {
 
 	switch req.Action {
 	case "auth":
-
+		app.Authenticate(w, req.Auth)
 	default:
 		app.errorJson(w, errors.New("invalid action"), http.StatusBadRequest)
 	}
+}
+
+func (app *Config) Authenticate(w http.ResponseWriter, data AuthPayload) {
+	jsonData, _ := json.MarshalIndent(data, "", "\t")
+
+	req, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusUnauthorized {
+		app.errorJson(w, errors.New("user Unauthorized"))
+		return
+	} else if res.StatusCode != http.StatusAccepted {
+		app.errorJson(w, errors.New("error when authorizing"))
+		return
+	}
+
+	var responseFromService response
+
+	err = json.NewDecoder(res.Body).Decode(&responseFromService)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	if responseFromService.Error {
+		app.errorJson(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	payload := response{
+		Error:   false,
+		Message: "auth completed",
+		Data:    responseFromService,
+	}
+	app.writeJson(w, http.StatusAccepted, payload)
 }
